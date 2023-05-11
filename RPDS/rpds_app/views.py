fa3be39be9753a996d6bbe79f411b52bea3061fc
BOSTONE069 <bostoneochieng69@gmail.com>
2023-05-10 20:21:19 -0400

    created urls and the different templates

--- RPDS/rpds_app/views.py
+++ RPDS/rpds_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def Home(request):
+    return render, "index.html"
+
+def rpds_app(request):
+    return  render, "rpds.html"
+
+def contact(request):
+    return render, "contact.html"
